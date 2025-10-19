88ec9577728771f3cfeab9ffcbc5b96b61892526
Dương Quang Thanh <thanhdqts00628@fpt.edu.vn>
2025-10-19 11:18:40 +0700

    Refactor repositories to extend JpaRepository for improved functionality

--- src/main/java/com/repository/UuDaiRepository.java
+++ src/main/java/com/repository/UuDaiRepository.java
@@ -1,7 +1,7 @@
 package com.repository;
 
 import com.model.UuDai;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UuDaiRepository extends Repository<UuDai, String> {
+public interface UuDaiRepository extends JpaRepository<UuDai, String> {
 }
