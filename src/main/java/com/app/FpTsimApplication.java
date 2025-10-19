3ce7bd0ca853e3b19becaf63a7a4e3a25938fc9c
Dương Quang Thanh <thanhdqts00628@fpt.edu.vn>
2025-10-19 11:36:15 +0700

    Enable scheduling and update component scan configuration in FpTsimApplication

--- src/main/java/com/app/FpTsimApplication.java
+++ src/main/java/com/app/FpTsimApplication.java
@@ -2,10 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com"})
+@SpringBootApplication(scanBasePackages = {"com"})
+@EnableScheduling
 public class FpTsimApplication {
     public static void main(String[] args) {
         SpringApplication.run(FpTsimApplication.class, args);
